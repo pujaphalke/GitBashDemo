e7cc694f631efa5b6f77cce0d355193a0f3e0bc6
pujaphalke <pujaphalke1997@gmail.com>
2025-10-01 15:56:46 +0530

    Update GitBashDemo.java
    added SOP statement.

--- GitBashDemo.java
+++ GitBashDemo.java
@@ -2,12 +2,9 @@ public class GitBashDemo
 {
  public static void main(String args[])
   {
-
     System.out.println("My First Git Bash Demo.");
-
-
-
-  }
+    System.out.println("Welcome Git Bash Demo.");
+   }
 
 
 
